f603b81d1df9303bb6898f44b8f17ca144ad285a
Thatsamanali00 <thatsamanali00@gmail.com>
2024-12-24 02:23:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,308 @@
+
+// Chapter 4
+// Q1
+
+// let variable1 = "Hellow", variable2 = 10, variable3 = true
+
+// Q2
+
+// 5 Legal Names
+
+// let username;
+// let $totalAmount;
+// let _index;
+// let myVariable123;
+// let camelCase;
+
+// Illegal Names
+
+// let 1stVariable; 
+// let my-variable; 
+// let function; 
+// let user name; 
+// let @value; 
+
+// Q3
+
+// document.write("<h1>Rules for namming Js</h1>")
+// document.write("<p>Variables name can only contains numbers,$,_, for example $my1st_Variable </p>")
+// document.write("<p> Variable must begin widh a letter,$ or _ for example $name, _name or name</p>")
+// document.write("<p>Variable names are case sensitive</p>")
+// document.write("<p>Variable names should not be Js Keyword</p>")
+
+// chapter 5
+
+// Q1
+
+// let num1=3
+// let num2=5
+// let sum=num1+num2
+
+// document.write(`sum of ${num1} and sum of ${num2} is ${sum}`)
+
+//Q2
+
+// Substraction
+
+ // let num1=5
+ // let num2=3
+ // let sum=num1-num2
+
+ // document.write(`sum of ${num1} and sum of ${num2} is ${sum}`)
+
+ // Multiplication
+
+ //  let num1=5
+ // let num2=3
+ //  let sum=num1*num2
+
+ // document.write(`sum of ${num1} and sum of ${num2} is ${sum}`)
+
+ // Division
+
+ // let num1=5
+ // let num2=3
+ // let sum=num1/num2
+
+ // document.write(`sum of ${num1} and sum of ${num2} is ${sum}`)
+
+ // modulus
+
+ // let num1=5
+ // let num2=3
+ // let sum=num1%num2
+
+ // document.write(`sum of ${num1} and sum of ${num2} is ${sum}`)
+
+// Q3
+
+// a
+
+ // let num ;
+
+// b  
+
+//  document.write(`<h1> “Value after variable declaration is ${num} </h1> `)
+
+// C
+
+// num=5
+
+// d
+
+// document.write(` <h1> “Initial value . ${num}” </h1> `)
+
+// e
+
+// num++;
+
+// f
+
+// document.write(`<h1> “Value after increment is ${num}” </h1> `)
+
+// g
+
+// num=num+7
+
+// h
+
+// document.write(`<h1> “Value after addition is ${num} ”. </h1> `)
+
+// i
+
+// num--;
+
+// j
+
+// document.write(` <h1> “Value after decrement is  ${num} ”. </h1> `)
+
+
+// k
+
+// num=num%3
+
+// l
+
+// document.write(`<h1> The reminder is ${num} </h1> `)
+
+// Q4
+
+// let num1 = 600
+// let num2 = 5
+// let sum = num1*num2
+
+// document.write(`<h1> The cost to buy ${num2} tickets is ${sum}PKR </h1>`)
+
+// Q5
+
+// let num1 = 4
+// let num2 = "x"
+// let num3 = 1
+
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1} </h1>  `)
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1*2} </h1>`)
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1*3} </h1>`)
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1*4} </h1>`)
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1*5} </h1>`)
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1*6} </h1>`)
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1*7} </h1>`)
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1*8} </h1>`)
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1*9} </h1>`)
+// document.write(` <h1> ${num1} ${num2} ${num3++}=${num1*10} </h1>`)
+
+
+// Q7
+
+// let a = 1
+// let b = 2
+// let c = 650
+// let d = 100
+// let e = 3
+// let f = 7
+// let g = 100
+
+
+// document.write(`<h1> price of item ${a} is ${c} </h1> `)
+// document.write(`<h1> Quantity of item ${a} is ${e} </h1> `)
+// document.write(`<h1> Price  of item ${b} is ${d} </h1> `)
+// document.write(`<h1> Quantity of item ${b} is ${f} </h1> `)
+// document.write(`<h1> Shipping Charges is ${d} </h1> `)
+
+
+// c=c*3;
+// d=700;
+
+// document.write(`<h1> Total cost of your order is ${c+d+g} </h1>`)
+
+// Q8
+
+// let totalMarks = 980
+// let = obtainedMarks = 804
+
+// document.write(`<h1> Total Marks ${totalMarks} </h1> `)
+// document.write(`<h1> Obatained Marks ${obtainedMarks} </h1> `)
+// document.write(`<h1> Percentage ${obtainedMarks/totalMarks*100} </h1> `)
+
+// Q9
+
+ // let usdToPkr=104.80
+ // let saudiRiyal=28
+ // let usdAmount=10
+ // let saudiRiyalAmount=25
+
+ // document.write(`<h1> PKR in Currency <h1> `)
+ // document.write(`<h1> ${usdToPkr*usdAmount + saudiRiyal*saudiRiyalAmount } <h1> `)
+
+// Q10
+
+//let number = 5
+//let add = 5
+//let multiply = 10
+//let devide = 2
+
+
+//document.write(`<h1>  ${number+add*multiply/devide} </h1> `)
+
+// Q11
+
+// let currentYear = 2024
+// let birthYear = 2006
+// let age1 = currentYear-birthYear
+// let age2 = age1 -1
+
+// document.write(` <h1> The person's age is either ${age1} or ${age2} years old. </h1>  `)
+
+// Q12 
+
+// let snack = "Chocolate Chip"
+// let age = 15
+// let maximumAge = 65
+// let amountSnackPerYear = 3
+// let remainingAge = 50
+
+
+// document.write(`<h1> You will need ${amountSnackPerYear*remainingAge} chocolate chip to last you until </h1> `)
+
+
+// Chapter 6
+
+// Q!
+
+// let num = 10
+
+// document.write(`<h1> Result the value of num is ${num} </h1> `)
+
+// document.write(`<h1> ................................... </h1> `)
+
+ // let preIncerement = ++num 
+
+ // document.write(`<h1> The value of ++num is  ${num} </h1>  `)
+ // document.write(`<h1> Now the value of num is  ${num} </h1> `)
+
+ // let postIncrement = num++
+
+ // document.write(`<h1> ................................... </h1> `)
+
+ // document.write(`<h1> The value of num++ is  ${num} </h1> `)
+ // document.write(`<h1> Now the value of num is  ${num} </h1> `)
+
+ // let preDecrement = --num
+
+// document.write(`<h1> ................................... </h1> `)
+
+ // document.write(`<h1> The value of --num is  ${num} </h1> `)
+ // document.write(`<h1> Now the value of num is  ${num} </h1> `)
+
+ // document.write(`<h1> ................................... </h1> `)
+
+
+ // let postIncerement = num--
+
+ 
+  // document.write(`<h1> The value of num-- is  ${num} </h1> `)
+  // document.write(`<h1> Now the value of num is  ${num} </h1> `)
+
+
+// Q2
+
+//let a = 2
+//let b = 1
+
+//let result = --a - --b + ++b + b--;
+
+// --a is 1
+// --a - --b is 1
+// --a - --b + ++b is 2
+// --a - --b + ++b + b-- is 3
+// result is 3
+
+
+//document.write(`${result}`)
+
+
+// Q6
+
+// let sub1 = "Urdu"
+// let sub2 = "English"
+// let sub3 = "Math"
+
+// let totalMarksEachSubject = 100
+// let subObtainedMarks1 = 54
+// let subObtainedMarks2 = 54
+// let subObtainedMarks3 = 48
+
+// let totalMarks = 300
+// let totalObtainedMarks = 156
+
+
+// document.write(`<h1> Subject Total Marks Obtained Percentage </h1>`)
+// document.write(`<h1> ${sub1} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${totalMarksEachSubject}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${subObtainedMarks1} &nbsp; &nbsp; &nbsp; &nbsp;   ${subObtainedMarks1/totalMarksEachSubject*totalMarksEachSubject}%</h1>`)
+// document.write(`<h1> ${sub2}  &nbsp; &nbsp; &nbsp; &nbsp; ${totalMarksEachSubject}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${subObtainedMarks2} &nbsp; &nbsp; &nbsp; &nbsp;   ${subObtainedMarks2/totalMarksEachSubject*totalMarksEachSubject}% </h1>`)
+// document.write(`<h1> ${sub3} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${totalMarksEachSubject}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${subObtainedMarks3} &nbsp; &nbsp; &nbsp; &nbsp;   ${subObtainedMarks3/totalMarksEachSubject*totalMarksEachSubject}% </h1>`)
+
+
+// document.write(`<h1> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  ${totalMarks} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${totalObtainedMarks} &nbsp; &nbsp; &nbsp; &nbsp; ${totalObtainedMarks/totalMarks*totalMarksEachSubject}% </h1> `)
+
+
+
+
